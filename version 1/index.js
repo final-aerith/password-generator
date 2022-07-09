1c436c976aeb0efdeb01ad65e419efc3e1cbe01d
Karen Avila <62681407+final-aerith@users.noreply.github.com>
2022-07-09 18:56:35 -0500

    Added first version of the project

---
+++ version 1/index.js
@@ -0,0 +1,18 @@
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"
+];
+
+const generatePassword = document.getElementById("generatePassword");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+console.log(generatePassword, password1, password2)
+
+generatePassword.addEventListener("click", function() {
+password1.textContent = characters.sort(() => 0.5 - Math.random()).slice(0, 15)
+password2.textContent = characters.sort(() => 0.5 - Math.random()).slice(0, 15)
+})
